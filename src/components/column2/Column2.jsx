361a81f94fed8fef2b9a8a8222eb08e797502afd
shernof <sherwin.nofuente@yahoo.com>
2024-02-29 07:00:31 +1100

    setup column1

--- src/components/column2/Column2.jsx
+++ src/components/column2/Column2.jsx
@@ -4,7 +4,9 @@ import classes from "./column2.module.css";
 export default function Column2() {
   return (
     <div className={classes.column2}>
-      <div className={classes.col2Container}>test</div>
+      <div className={classes.col2Container}>
+        <div></div>
+      </div>
     </div>
   );
 }
